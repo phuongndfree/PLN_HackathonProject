cbdc3e7c75f8481c22329746ea33b2f950282ea5
phuongnd <phuongndfree@gmail.com>
2016-03-27 10:56:42 +0700

    final

--- Setup/Setup/mainForm.Designer.cs
+++ Setup/Setup/mainForm.Designer.cs
@@ -28,33 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.cbxPhuongXa = new System.Windows.Forms.ComboBox();
             this.lbPhuongXa = new System.Windows.Forms.Label();
             this.btFilter = new System.Windows.Forms.Button();
             this.pnFilter = new System.Windows.Forms.Panel();
             this.pnGridView = new System.Windows.Forms.Panel();
+            this.chartNew = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dgvFilter = new System.Windows.Forms.DataGridView();
             this.pnResult = new System.Windows.Forms.Panel();
-            this.lbResult = new System.Windows.Forms.Label();
-            this.txtGF1 = new System.Windows.Forms.TextBox();
-            this.txtGF2 = new System.Windows.Forms.TextBox();
-            this.txtGF3 = new System.Windows.Forms.TextBox();
-            this.txtGF4 = new System.Windows.Forms.TextBox();
-            this.btCRF = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.lbSugestion = new System.Windows.Forms.Label();
-            this.chartNew = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGF4 = new System.Windows.Forms.TextBox();
+            this.txtGF3 = new System.Windows.Forms.TextBox();
+            this.txtGF2 = new System.Windows.Forms.TextBox();
+            this.txtGF1 = new System.Windows.Forms.TextBox();
+            this.lbResult = new System.Windows.Forms.Label();
             this.pnFilter.SuspendLayout();
             this.pnGridView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartNew)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFilter)).BeginInit();
             this.pnResult.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chartNew)).BeginInit();
             this.SuspendLayout();
             // 
             // cbxPhuongXa
@@ -80,7 +78,7 @@
             this.btFilter.Name = "btFilter";
             this.btFilter.Size = new System.Drawing.Size(75, 23);
             this.btFilter.TabIndex = 2;
-            this.btFilter.Text = "Filter";
+            this.btFilter.Text = "Du bao";
             this.btFilter.UseVisualStyleBackColor = true;
             this.btFilter.Click += new System.EventHandler(this.btFilter_Click);
             // 
@@ -105,24 +103,35 @@
             this.pnGridView.Size = new System.Drawing.Size(817, 148);
             this.pnGridView.TabIndex = 4;
             // 
+            // chartNew
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartNew.ChartAreas.Add(chartArea2);
+            this.chartNew.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chartNew.Legends.Add(legend2);
+            this.chartNew.Location = new System.Drawing.Point(596, 0);
+            this.chartNew.Name = "chartNew";
+            this.chartNew.Size = new System.Drawing.Size(221, 148);
+            this.chartNew.TabIndex = 12;
+            this.chartNew.Text = "CHART";
+            // 
             // dgvFilter
             // 
             this.dgvFilter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvFilter.Dock = System.Windows.Forms.DockStyle.Left;
             this.dgvFilter.Location = new System.Drawing.Point(0, 0);
             this.dgvFilter.Name = "dgvFilter";
-            this.dgvFilter.Size = new System.Drawing.Size(508, 148);
+            this.dgvFilter.Size = new System.Drawing.Size(596, 148);
             this.dgvFilter.TabIndex = 0;
             // 
             // pnResult
             // 
-            this.pnResult.Controls.Add(this.lbSugestion);
             this.pnResult.Controls.Add(this.label5);
             this.pnResult.Controls.Add(this.label4);
             this.pnResult.Controls.Add(this.label3);
             this.pnResult.Controls.Add(this.label2);
             this.pnResult.Controls.Add(this.label1);
-            this.pnResult.Controls.Add(this.btCRF);
             this.pnResult.Controls.Add(this.txtGF4);
             this.pnResult.Controls.Add(this.txtGF3);
             this.pnResult.Controls.Add(this.txtGF2);
@@ -134,79 +143,14 @@
             this.pnResult.Size = new System.Drawing.Size(817, 262);
             this.pnResult.TabIndex = 5;
             // 
-            // lbResult
+            // label5
             // 
-            this.lbResult.AutoSize = true;
-            this.lbResult.Location = new System.Drawing.Point(109, 76);
-            this.lbResult.Name = "lbResult";
-            this.lbResult.Size = new System.Drawing.Size(37, 13);
-            this.lbResult.TabIndex = 0;
-            this.lbResult.Text = "Predic";
-            // 
-            // txtGF1
-            // 
-            this.txtGF1.Location = new System.Drawing.Point(86, 6);
-            this.txtGF1.Name = "txtGF1";
-            this.txtGF1.Size = new System.Drawing.Size(100, 20);
-            this.txtGF1.TabIndex = 1;
-            // 
-            // txtGF2
-            // 
-            this.txtGF2.Location = new System.Drawing.Point(192, 6);
-            this.txtGF2.Name = "txtGF2";
-            this.txtGF2.Size = new System.Drawing.Size(100, 20);
-            this.txtGF2.TabIndex = 2;
-            // 
-            // txtGF3
-            // 
-            this.txtGF3.Location = new System.Drawing.Point(298, 6);
-            this.txtGF3.Name = "txtGF3";
-            this.txtGF3.Size = new System.Drawing.Size(100, 20);
-            this.txtGF3.TabIndex = 3;
-            // 
-            // txtGF4
-            // 
-            this.txtGF4.Location = new System.Drawing.Point(404, 6);
-            this.txtGF4.Name = "txtGF4";
-            this.txtGF4.Size = new System.Drawing.Size(100, 20);
-            this.txtGF4.TabIndex = 4;
-            // 
-            // btCRF
-            // 
-            this.btCRF.Location = new System.Drawing.Point(16, 66);
-            this.btCRF.Name = "btCRF";
-            this.btCRF.Size = new System.Drawing.Size(75, 23);
-            this.btCRF.TabIndex = 5;
-            this.btCRF.Text = "Predic";
-            this.btCRF.UseVisualStyleBackColor = true;
-            this.btCRF.Click += new System.EventHandler(this.btCRF_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Informations:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(86, 33);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Hoc Van %";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(189, 33);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Co viec lam %";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Sinh tai Binh Duong %";
             // 
             // label4
             // 
@@ -217,36 +161,70 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "Ko co TATS %";
             // 
-            // label5
+            // label3
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(401, 33);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Goc Ngoai TInh %";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Co viec lam %";
             // 
-            // lbSugestion
+            // label2
             // 
-            this.lbSugestion.AutoSize = true;
-            this.lbSugestion.Location = new System.Drawing.Point(442, 76);
-            this.lbSugestion.Name = "lbSugestion";
-            this.lbSugestion.Size = new System.Drawing.Size(66, 13);
-            this.lbSugestion.TabIndex = 11;
-            this.lbSugestion.Text = "Suggestion: ";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hoc Van %";
             // 
-            // chartNew
+            // label1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartNew.ChartAreas.Add(chartArea1);
-            this.chartNew.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chartNew.Legends.Add(legend1);
-            this.chartNew.Location = new System.Drawing.Point(508, 0);
-            this.chartNew.Name = "chartNew";
-            this.chartNew.Size = new System.Drawing.Size(309, 148);
-            this.chartNew.TabIndex = 12;
-            this.chartNew.Text = "CHART";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Thong tin";
+            // 
+            // txtGF4
+            // 
+            this.txtGF4.Location = new System.Drawing.Point(404, 6);
+            this.txtGF4.Name = "txtGF4";
+            this.txtGF4.Size = new System.Drawing.Size(100, 20);
+            this.txtGF4.TabIndex = 4;
+            // 
+            // txtGF3
+            // 
+            this.txtGF3.Location = new System.Drawing.Point(298, 6);
+            this.txtGF3.Name = "txtGF3";
+            this.txtGF3.Size = new System.Drawing.Size(100, 20);
+            this.txtGF3.TabIndex = 3;
+            // 
+            // txtGF2
+            // 
+            this.txtGF2.Location = new System.Drawing.Point(192, 6);
+            this.txtGF2.Name = "txtGF2";
+            this.txtGF2.Size = new System.Drawing.Size(100, 20);
+            this.txtGF2.TabIndex = 2;
+            // 
+            // txtGF1
+            // 
+            this.txtGF1.Location = new System.Drawing.Point(86, 6);
+            this.txtGF1.Name = "txtGF1";
+            this.txtGF1.Size = new System.Drawing.Size(100, 20);
+            this.txtGF1.TabIndex = 1;
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.Location = new System.Drawing.Point(109, 76);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(59, 20);
+            this.lbResult.TabIndex = 0;
+            this.lbResult.Text = "Predic";
             // 
             // mainForm
             // 
@@ -261,10 +239,10 @@
             this.pnFilter.ResumeLayout(false);
             this.pnFilter.PerformLayout();
             this.pnGridView.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartNew)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFilter)).EndInit();
             this.pnResult.ResumeLayout(false);
             this.pnResult.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chartNew)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -279,7 +257,6 @@
         private System.Windows.Forms.DataGridView dgvFilter;
         private System.Windows.Forms.Panel pnResult;
         private System.Windows.Forms.Label lbResult;
-        private System.Windows.Forms.Button btCRF;
         private System.Windows.Forms.TextBox txtGF4;
         private System.Windows.Forms.TextBox txtGF3;
         private System.Windows.Forms.TextBox txtGF2;
@@ -289,7 +266,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lbSugestion;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartNew;
     }
 }
